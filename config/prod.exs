c9894641da0b3624090c09aa78665da7eed121ff
Wildan Frananda <wildanrailfanskrl@gmail.com>
2026-09-20 15:04:59 +0700

    chore: remove unnecessary blank line in production configuration

--- config/prod.exs
+++ config/prod.exs
@@ -1,6 +1,5 @@
 import Config
 
-
 # Force using SSL in production. This also sets the "strict-security-transport" header,
 # known as HSTS. If you have a health check endpoint, you may want to exclude it below.
 # Note `:force_ssl` is required to be set at compile-time.
